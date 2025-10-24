8860cf3e177531e469156ba7f6e78e0e5494b8b7
FrethLopez <zepolfz18@gmail.com>
2025-10-24 09:44:20 -0500

    Subida del Proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './pages/inicio/inicio';
+import { MainLayout } from './shared/layout/main-layout/main-layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Inicio },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'consultas',
+        loadComponent: () =>
+          import('./pages/consultas/consultas').then(
+            m => m.Consultas
+          ),
+      },
+      {
+        path: 'reportes',
+        loadComponent: () =>
+          import('./pages/reportes/reportes').then(
+            m => m.Reportes
+          ),
+      },
+      {
+        path: 'configuracion',
+        loadComponent: () =>
+          import('./pages/configuracion/configuracion').then(
+            m => m.Configuracion
+          ),
+      },
+    ],
+  },
+  { path: '**', redirectTo: '' },
+];
